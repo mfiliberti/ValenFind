bfc392e26bd16d4d9c722f81f1094171a36cb971
Matthew Filiberti <mfiliberti@email.arizona.edu>
2017-12-24 02:25:22 -0700

    Working example for one test case

--- ValenFindScript.py
+++ ValenFindScript.py
@@ -14,7 +14,6 @@ class Person:
         self.gender = gender
         self.answers = answers
         self.preferenceList = []
-        self.engagedTo = "none"
 
 #Parse CSV file (example people for now)
 p1 = Person("graham", "male", [4, 4, 0, 0, 4])
@@ -39,13 +38,13 @@ women = [p6, p7, p8, p9, p10]
 #men
 for x in range(0, len(men)):
         preferenceList = []
-        mAnswers = men[x].answers
+        mAnswers = men[x]
         for y in range(0, len(women)):
             wAnswers = women[y].answers
             total = 0
             for z in range(0, len(mAnswers)):
-                total = total + (abs(int(mAnswers[z])-int(wAnswers[z])))
-            preferenceList.append((women[y], total))
+                total = total + (abs(mAnswers[z]-wAnswers[z]))
+            preferenceList.append({women[y], total})
         men[x].preferenceList = preferenceList
 #sort
 for x in range(0, len(men)):
@@ -54,63 +53,17 @@ for x in range(0, len(men)):
 #women
 for x in range(0, len(women)):
         preferenceList = []
-        wAnswers = women[x].answers
+        wAnswers = women[x]
         for y in range(0, len(men)):
             mAnswers = men[y].answers
             total = 0
             for z in range(0, len(wAnswers)):
                 total = total + (abs(wAnswers[z]-mAnswers[z]))
-            preferenceList.append((men[y], total))
-        women[x].preferenceList = preferenceList
+            preferenceList.append({men[y], total})
+        men[x].preferenceList = preferenceList
 #sort
 for x in range(0, len(men)):
     women[x].preferenceList = sorted(women[x].preferenceList, key=lambda y: y[1])
-
-#apply algorithm to men and women lists
-allEngaged = 0
-while allEngaged == 0:
-    #Round of proposals, men to women
-    for x in range(0, len(men)):
-        #If not engaged, try best woman
-        if men[x].engagedTo == "none":
-            men[x].engagedTo = men[x].preferenceList[0][0].name
-    #Women deny every man who is not best
-    for x in range(0, len(women)):
-        proposed = []
-        #get proposed
-        for y in range(0, len(men)):
-            if men[y].engagedTo == women[x].name:
-                proposed.append(men[y])
-        #find highest ranked proposal and engage, deny all others
-        bestName = ""
-        bestIndex = 999999
-        for y in range(0, len(proposed)):
-            index = [dude[0].name for dude in women[x].preferenceList].index(proposed[y].name)
-            if index < bestIndex:
-                bestIndex = index
-                bestName = proposed[y].name
-        women[x].engagedTo = bestName
-        for y in range(0, len(proposed)):
-            if proposed[y].name != bestName:
-                proposed[y].engagedTo = "none"
-                proposed[y].preferenceList.pop(0)
-    #Check if everyone is engaged
-    for x in range(0, len(men)):
-        allEngaged = 1
-        if men[x].engagedTo == "none":
-            allEngaged = 0
-            break
-
-#print results
-for x in range(0, len(men)):
-    print men[x].name + " should marry " + men[x].engagedTo
-
-
-
-
-
-
-
 
 
 
